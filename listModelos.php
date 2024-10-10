d0c22ac3fdcd08a3d7296a9b719f979d8342042e
s1nistr0 <guilherme.svicenti@gmail.com>
2024-10-10 12:32:46 -0300

    teste img

---
+++ listModelos.php
@@ -0,0 +1,30 @@
+<?php
+$directory = './images/MODELOS';
+$modelos = [];
+
+if (is_dir($directory)) {
+    $dirs = scandir($directory);
+    foreach ($dirs as $dir) {
+        if ($dir !== '.' && $dir !== '..') {
+            $modelDir = "$directory/$dir";
+            if (is_dir($modelDir)) {
+                $files = scandir($modelDir);
+                foreach ($files as $file) {
+                    if (pathinfo($file, PATHINFO_EXTENSION) === 'jpg') {
+                        $imagePath = "$modelDir/$file";
+                        $modelos[] = [
+                            'id' => $dir,
+                            'name' => $dir,
+                            'image' => $imagePath
+                        ];
+                        break; // Assume que há apenas uma imagem por modelo
+                    }
+                }
+            }
+        }
+    }
+}
+
+header('Content-Type: application/json');
+echo json_encode($modelos);
+?>
